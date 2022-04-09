57bf16796db671ad6472f1192cf83f6e3e33f8e0
darshilkantaria11 <91153906+darshilkantaria11@users.noreply.github.com>
2022-04-09 22:44:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Home from "./js/Home";
+import Second from "./js/Second";
+import Rotate from "./js/Rotate";
+import Liteabout from "./js/Liteabout";
+
+function App() {
+  return (
+   <>
+       <Home/>
+       <Second/>
+       <Rotate/>
+       <Liteabout/>
+   </>
+  );
+}
+
+export default App;
